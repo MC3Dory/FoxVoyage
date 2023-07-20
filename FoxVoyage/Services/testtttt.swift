700e2ef19ba7f3aa4abfe0c1e3e1f60713b5d8d3
fbrmdhni <134994755+fbrmdhni@users.noreply.github.com>
2023-07-20 12:10:13 +0700

    fix foldering

---
+++ FoxVoyage/Services/testtttt.swift
@@ -0,0 +1,20 @@
+//
+//  testtttt.swift
+//  FoxVoyage
+//
+//  Created by Febrina Yusuf on 20/07/23.
+//
+
+import SwiftUI
+
+struct testtttt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testtttt_Previews: PreviewProvider {
+    static var previews: some View {
+        testtttt()
+    }
+}
