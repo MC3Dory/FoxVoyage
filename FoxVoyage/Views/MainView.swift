dcf32e1e35ff141cae2e32813464811e4ee95b44
nindyalita <118523607+nindyalita@users.noreply.github.com>
2023-07-21 00:10:19 +0700

    fix foldering

---
+++ FoxVoyage/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  FoxVoyage
+//
+//  Created by Nindya Alita Rosalia on 21/07/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hai")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
