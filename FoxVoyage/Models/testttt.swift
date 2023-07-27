c7454192a2937e9d4d52409265bb3a93789bd084
nindyalita <118523607+nindyalita@users.noreply.github.com>
2023-07-27 18:14:46 +0700

    Revert "merge commit"
    This reverts commit e6c8aaa945103e0aa64447a03cffd1dd193d64fa.

---
+++ FoxVoyage/Models/testttt.swift
@@ -0,0 +1,20 @@
+//
+//  testttt.swift
+//  FoxVoyage
+//
+//  Created by Febrina Yusuf on 20/07/23.
+//
+
+import SwiftUI
+
+struct testttt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testttt_Previews: PreviewProvider {
+    static var previews: some View {
+        testttt()
+    }
+}
