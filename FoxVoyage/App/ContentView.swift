81f7279a641ccb0d1374ea09fdf612f1fde53215
nindyalita <118523607+nindyalita@users.noreply.github.com>
2023-07-30 18:43:42 +0700

    update after merge

--- FoxVoyage/App/ContentView.swift
+++ FoxVoyage/App/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
             OnboardingView()
             
         }else{
-            JourneyView()
+            MainView()
         }
             
 
--- FoxVoyage/Views/AddNoteToPhotoView.swift
+++ FoxVoyage/Views/AddNoteToPhotoView.swift
@@ -25,7 +25,6 @@ struct AddNoteToPhotoView: View {
                         .scaledToFill()
                         .ignoresSafeArea()
                 }
-                
                 VStack{
                     HStack{
                         Spacer()
